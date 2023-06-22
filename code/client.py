41338626f8d6821b1d0e9dd71c4c990f892a61f1
Shwam <williamshawnmccormick@gmail.com>
2023-06-22 10:41:28 -0600

    Create sample client

---
+++ code/client.py
@@ -0,0 +1,67 @@
+from socket import *
+from struct import pack
+import multiprocessing, threading
+from queue import Empty
+RECV_SIZE=4096
+
+class DarknetClient:
+
+    def __init__(self, server_ip, server_port, image_queue, result_queue):
+        self.socket = None
+        self.connect(server_ip, server_port)
+        self.image_queue = image_queue
+        self.result_queue = result_queue
+
+    def connect(self, server_ip, server_port):
+        self.socket = socket(AF_INET, SOCK_STREAM)
+        self.socket.connect((server_ip, server_port))
+    
+    def run(self):
+        data = b""
+        while True:
+            try:
+                image = self.image_queue.get(block=False)
+                if image == None: # quit command
+                    self.close()
+                    break
+                if image:
+                    # send it to the server
+                    self.send_image(image)
+                data = self.socket.recv(RECV_SIZE)
+                if data:
+                    self.result_queue.put(data.decode("utf8"))
+            except Empty:
+                pass
+        
+    def close(self):
+        # send the quit command
+        self.socket.sendall(pack('>q', 0))
+        # shut down
+        self.socket.shutdown(SHUT_WR)
+        self.socket.close()
+        self.socket = None
+
+    def send_image(self, image_data):
+        length = pack('>Q', len(image_data))
+        self.socket.sendall(length)
+        self.socket.sendall(image_data)
+
+if __name__ == '__main__':
+    """ Sample client which performs detection on a single image and then disconnects """
+    # Start the client thread
+    image_queue = multiprocessing.Queue() # put images to be processed
+    result_queue = multiprocessing.Queue() # receive detections
+    client = DarknetClient('localhost', 7061, image_queue, result_queue)
+    threading.Thread(target=DarknetClient.run, args=(client,)).start()
+
+    # Queue an image to be processed
+    with open('images/test.jpg', 'rb') as f:
+        image_data = f.read()    
+        image_queue.put(image_data)
+
+    # Get the resulting detection
+    result = result_queue.get(block=True, timeout=None)
+    print("Got detection", result)
+
+    # Tell the client to shut down
+    image_queue.put(None)
---
+++ code/util.py
@@ -0,0 +1,39 @@
+def save_xml(boxes, filename):
+    width = 2560
+    height = 1440
+    path = f"/output/{filename}"
+    object_template = lambda label, rect: f"""<object>
+        <name>{label}</name>
+        <pose>Unspecified</pose>
+        <truncated>0</truncated>
+        <difficult>0</difficult>
+        <bndbox>
+            <xmin>{int((rect[0]-rect[2]/2)*width)}</xmin>
+            <ymin>{int((rect[1]-rect[3]/2)*height)}</ymin>
+            <xmax>{int((rect[0]+rect[2]/2)*width)}</xmax>
+            <ymax>{int((rect[1]+rect[3]/2)*height)}</ymax>
+        </bndbox>
+    </object>"""
+    objects = ""
+    for box in boxes:
+        label, confidence, rect = box    
+        objects += object_template(label, rect) + "\n"
+                
+    output = f"""<annotation>
+        <folder>images</folder>
+        <filename>{filename}</filename>
+        <path>{path}</path>
+        <source>
+            <database>Unknown</database>
+        </source>
+        <size>
+            <width>{width}</width>
+            <height>{height}</height>
+            <depth>3</depth>
+        </size>
+        <segmented>0</segmented>
+        {objects}
+    </annotation>"""
+
+    with open(path.replace(".jpg", ".xml"), "w") as f:
+        f.write(output)
