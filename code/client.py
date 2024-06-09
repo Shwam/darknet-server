b8578d258fccdbecab1bbdc1f6965450f79fd4f8
Shwam <williamshawnmccormick@gmail.com>
2024-06-09 13:30:11 -0600

    Cache results on client side

--- code/client.py
+++ code/client.py
@@ -7,6 +7,7 @@ import time
 
 RECV_SIZE=4096
 
+
 class DarknetClient:
 
     def __init__(self, server_ip, server_port, image_queue, result_queue):
@@ -15,6 +16,7 @@ class DarknetClient:
         self.server_port = server_port
         self.image_queue = image_queue
         self.result_queue = result_queue
+        self.detection_cache = Cache()
 
     def connect(self):
         retries = 0
@@ -38,28 +40,36 @@ class DarknetClient:
             if image is None: # quit command
                 self.close()
                 return 0
-            # send it to the server
-            try:
-                self.send_image(image)
-            except Exception as err:
-                print(f"Error communicating with image server {err}. Retrying connection")
-                self.connect()
-                self.image_queue.put(image)
-                continue
-            data = b""
-            while not data:
+            if image in self.detection_cache:
+                # No need to process it, just send the result back
+                detection = self.detection_cache[image]
+                self.result_queue.put(detection)
+
+            else:
+                # send it to the server
                 try:
-                    data = self.socket.recv(RECV_SIZE)
-                    if not data:
-                        print(f"Error communicating with image server (Empty data). Retrying connection")
-                        self.connect()
-                        break
+                    self.send_image(image)
                 except Exception as err:
                     print(f"Error communicating with image server {err}. Retrying connection")
                     self.connect()
-                    break
+                    self.image_queue.put(image)
+                    continue
+                data = b""
+                while not data:
+                    try:
+                        data = self.socket.recv(RECV_SIZE)
+                        if not data:
+                            print(f"Error communicating with image server (Empty data). Retrying connection")
+                            self.connect()
+                            break
+                    except Exception as err:
+                        print(f"Error communicating with image server {err}. Retrying connection")
+                        self.connect()
+                        break
                 if data:
-                    self.result_queue.put(ast.literal_eval(data.decode("utf8")))
+                    detection = ast.literal_eval(data.decode("utf8"))
+                    self.result_queue.put(detection)
+                    self.detection_cache.put(image, detection)
                 else:
                     print("No data returned from server")
                     # Try it again
@@ -77,6 +87,35 @@ class DarknetClient:
         length = pack('>Q', len(image_data))
         self.socket.sendall(length)
         self.socket.sendall(image_data)
+
+class Cache:
+    def __init__(self, max_size=10):
+        self.q = []
+        self.cache = dict()
+        self.max_size = max_size
+
+    def get(self, data):
+        if data in self.cache:
+            self.q.remove(data)
+            self.q.insert(0, data)
+        return self.cache.get(data, None)
+
+    def put(self, item, result):
+        if item in self.cache:
+            self.q.remove(item)
+        else:
+            self.cache[item] = result
+        self.q.insert(0, item)
+        if len(self.q) > self.max_size:
+            last_item = self.q[self.max_size]
+            del self.cache[last_item]
+            del self.q[self.max_size]
+
+    def __contains__(self, item):
+        return item in self.cache
+
+    def __getitem__(self, key):
+        return self.get(key)
 
 if __name__ == '__main__':
     """ Sample client which performs detection on a single image and then disconnects """
